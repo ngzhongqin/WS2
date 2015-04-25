346cea62cc6b5891fceec31be97db097f4d4c606
ngzhongqin <ngzhongqin@gmail.com>
2015-04-25 22:44:29 +0800

    logo responsive topbar banner

--- app/assets/javascripts/home/banner/banner.js
+++ app/assets/javascripts/home/banner/banner.js
@@ -6,10 +6,11 @@ $(document).ready(function() {
 
     $("#owl-demo").owlCarousel({
 
-        navigation : true, // Show next and prev buttons
+        navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
+        singleItem:true,
+        autoPlay : true
 
         // "singleItem:true" is a shortcut for:
         // items : 1,
